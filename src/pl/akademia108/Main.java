91d32c1792ea8dd2a88e788dffd0b41516f547f3
Krystian Wieczorek <krystianwieczorek70@gmail.com>
2021-12-21 10:30:36 +0100

    Commit

--- src/pl/akademia108/Main.java
+++ src/pl/akademia108/Main.java
@@ -1,8 +1,16 @@
 package pl.akademia108;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Wprowadz kurs USD: ");
+        Scanner scan = new Scanner(System.in);
+
+        Double kurs = Double.valueOf(scan.next());
+        Double mnozonyKurs = 100 * kurs;
+
+        System.out.println(kurs);
+        System.out.println(mnozonyKurs);
     }
 }
